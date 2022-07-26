19127d5158af834987559bb4c353d79b9af92485
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 15:23:15 -0600

    string methods

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 ///////////////////////////////////////
-// Working With Strings Intro
+// String Methods
 
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+    const [type, from, to, time] = flight.split(';');
+    const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+        '_',
+        ' '
+    )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+    console.log(output);
+}
+
+
+
+///////////////////////////////////////
+// Working With Strings Intro
+/*
 // Split and Join
 console.log('a+very+nice+string'.split('+'));
 console.log('Kelsy Watkins'.split(' '));
@@ -112,7 +136,7 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
-
+*/
 
 
 ///////////////////////////////////////
