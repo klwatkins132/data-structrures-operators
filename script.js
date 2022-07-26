dc68b1151040660a206e01b753645cf1aaafb1c9
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 13:39:32 -0600

    The Spread Operator

--- script.js
+++ script.js
@@ -1,6 +1,97 @@
 ///////////////////////////////////////
-// Destructuring Objects
+// The Spread Operator (...)
 
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// Note: spread operator takes all values out of array and write them indivigually
+const goodNewArr = [1, 2, ...arr];
+console.log(goodNewArr);
+
+console.log(...goodNewArr); // same as writing : console.log(1, 2, 7, 8, 9);
+
+const restaurant = {
+    //key: value
+    Rname: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}`);
+    }
+};
+
+//builds new array, creates a shallow copy
+//spread operator takes all the elements from array, doesn't create new variables
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];  // adds gnocci to end of array, this creates new array but doesnt affect origional array
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Schmedtmann`); // // templete literals don't expect multiple values seperated by a comma, will not work here
+
+// Real-world example
+const ingredients = [
+    prompt("Let's make pasta! Ingredient 1?"),
+    prompt('Ingredient 2?'),
+    prompt('Ingredient 3'),
+];
+console.log(ingredients);
+
+// Yields the exact same result
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.Rname = 'Ristorante Roma';
+console.log(restaurantCopy.Rname);
+console.log(restaurant.Rname);
+
+
+
+///////////////////////////////////////
+// Destructuring Objects
+/*
 const restaurant = {
     Rname: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -70,7 +161,7 @@ const {
     fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
-
+*/
 
 
 ///////////////////////////////////////
@@ -122,4 +213,5 @@ console.log(i, j, k);
 
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r); */
+console.log(p, q, r); 
+*/
