5392bef5eb5f0538093f31099572b619c257f950
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 14:03:54 -0600

    Short Circuiting, Nullish Coalescing Operator

--- script.js
+++ script.js
@@ -1,6 +1,135 @@
 ///////////////////////////////////////
+// The Nullish Coalescing Operator
+const restaurant = {
+    Rname: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}`);
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10; // restaurant.numGuests is falsey because 0 is falsey value
+console.log(guests);
+
+// nullish values are null and undefined(NOT INCLUDE 0 or '')
+// only nullish values will short circut the evulation
+const guestCorrect = restaurant.numGuests ?? 10; // restaurant.numGuests is 0 because the nullish coalescing operator works with nullish values instead of falsey values
+console.log(guestCorrect);
+
+
+
+///////////////////////////////////////
+// Short Circuiting (&& and ||)
+// OR OPERATOR SHORT CIRCUTS WHEN FIRST VALUE IS TRUTHY -- WILL RETURN FIRST TRUTHY VALUE -- OR RETURN LAST VALUE IF ALL ARE FALSEY
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Kelsy'); // returns three which is a truthy value, will not even look at 'Kelsy'
+console.log('' || 'Kelsy'); // prints 'Kelsy'
+console.log(true || 0); // prints true
+console.log(undefined || null); // prints null, if both values falsey it will print last value
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); // prints hello as its the first truthy value it finds, short circuts and doesnt look at the rest
+
+const restaurant = {
+    Rname: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}`);
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;  // does resturants.numGuests exist? if not default it to 10
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;  // value will be 10 as numGuests is undefined
+console.log(guests2);
+
+// AND OPERATOR SHORT CIRCUTS WHEN FIRST VALUE IS FALSEY -- WILL RETURN FIRST FALSEY VALUE -- OR RETURN LAST VALUE IF ALL ARE TRUTHY
+console.log('---- AND ----');
+console.log(0 && 'Kelsy');  // prints 0
+console.log(7 && 'Kelsy');  // prints 'Kelsy'
+console.log('Hello' && 23 && null && 'Kelsy');  // prints null
+
+// Practical example
+if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+//same as above if block, && operator checks to see if first statement is falsey, if it is it will short circut
+// so does .orderPizza exist? if yes then call function
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+
+
+///////////////////////////////////////
 // Rest Pattern and Parameters
 // looks like spread operator but does the exact opposite -- pack elements into an array
+/*
 // 1) Destructuring
 
 // SPREAD, because on RIGHT side of =
@@ -77,7 +206,7 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms'); // produces an empty array
-
+*/
 
 
 ///////////////////////////////////////
@@ -141,7 +270,7 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log(menu);
 
 // Iterables: arrays, strings, maps, sets. NOT objects
-const str = 'Jonas';
+const str = 'Kelsy';
 const letters = [...str, ' ', 'S.'];
 console.log(letters);
 console.log(...str);
