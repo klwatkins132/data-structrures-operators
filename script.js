7632efcbcc1c6c392428c24e1d869d7f9cef1352
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 14:41:59 -0600

    Optional chaining

--- script.js
+++ script.js
@@ -1,5 +1,83 @@
 ///////////////////////////////////////
+// Optional Chaining
+const weekdays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0,
+        close: 24,
+    },
+};
+
+const restaurant = {
+    //key: value
+    Rname: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(
+            `Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+        );
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}`);
+    },
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+if (restaurant.openingHours && restaurant.openingHours.mon) //checking if monday openhours exists
+    console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+    const open = restaurant.openingHours[day]?.open ?? '**CLOSED';
+    console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+// Same as above but less efficient 
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
+
+
+
+///////////////////////////////////////
 // The Nullish Coalescing Operator
+/*
 const restaurant = {
     Rname: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -123,7 +201,7 @@ if (restaurant.orderPizza) {
 //same as above if block, && operator checks to see if first statement is falsey, if it is it will short circut
 // so does .orderPizza exist? if yes then call function
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
+*/
 
 
 ///////////////////////////////////////
