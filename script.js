09ee3f2e08044fef50944d166274c4202bacc0b8
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 15:18:34 -0600

    strings intro

--- script.js
+++ script.js
@@ -1,5 +1,123 @@
 ///////////////////////////////////////
+// Working With Strings Intro
+
+// Split and Join
+console.log('a+very+nice+string'.split('+'));
+console.log('Kelsy Watkins'.split(' '));
+
+const [firstName, lastName] = 'Kelsy Watkins'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+    const names = name.split(' ');
+    const namesUpper = [];
+
+    for (const n of names) {
+        // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+        namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+    }
+    console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('kelsy watkins');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Kelsy'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+    const str = number + '';
+    const last = str.slice(-4);
+    return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+    console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+
+///////////////////////////////////////
+// Working With Strings Intro - Continued
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'kElSy'; // Kelsy
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+    passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = 'hello@kelsy.io';
+const loginEmail = '  Hello@Kelsy.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+    'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+    console.log('Part of the NEW ARirbus family');
+}
+
+
+const checkBaggage = function (items) {
+    const baggage = items.toLowerCase();
+
+    if (baggage.includes('knife') || baggage.includes('gun')) {
+        console.log('You are NOT allowed on board');
+    } else {
+        console.log('Welcome aboard!');
+    }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+
+
+///////////////////////////////////////
 // Maps: Iteration
+/*
 const question = new Map([
     ['question', 'What is the best programming language in the world?'],
     [1, 'C'],
@@ -126,7 +244,7 @@ console.log(
 );
 
 console.log(new Set('kelsywatkins').size); // prints 10 -- There are 10 unique values
-
+*/
 
 
 ///////////////////////////////////////
@@ -327,7 +445,7 @@ console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
 console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
 // Arrays
-const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+const users = [{ name: 'Kelsy', email: 'hello@Kelsy.io' }];
 // const users = [];
 
 console.log(users[0]?.name ?? 'User array empty');
@@ -615,7 +733,7 @@ const str = 'Kelsy';
 const letters = [...str, ' ', 'S.'];
 console.log(letters);
 console.log(...str);
-// console.log(`${...str} Schmedtmann`); // // templete literals don't expect multiple values seperated by a comma, will not work here
+// console.log(`${...str} Watkins`); // // templete literals don't expect multiple values seperated by a comma, will not work here
 
 // Real-world example
 const ingredients = [
