2236f3e83051c3ff1f24cfdd55e3c32056243380
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 13:24:50 -0600

    Destructuring Objects

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
 ///////////////////////////////////////
-// Destructuring Arrays
+// Destructuring Objects
 
+const restaurant = {
+    Rname: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    }
+};
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+    address: 'Via del Sole, 21',
+    starterIndex: 1,
+});
+
+const { Rname, openingHours, categories } = restaurant;
+console.log(Rname, openingHours, categories);
+
+const {
+    Rname: restaurantName,
+    openingHours: hours,
+    categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+    fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+
+
+///////////////////////////////////////
+// Destructuring Arrays
+/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -47,4 +122,4 @@ console.log(i, j, k);
 
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+console.log(p, q, r); */
